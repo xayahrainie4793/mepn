d721901dfa2bb23a15b56295383d9910f04600ca
xayahrainie4793 <66528374+xayahrainie4793@users.noreply.github.com>
2021-07-14 19:29:15 +0800

    Update minimal.c

--- minimal.c
+++ minimal.c
@@ -275,7 +275,7 @@ int isprime(char* p)
 {	mpz_t temp;
 	mpz_init(temp);
 	mpz_set_str(temp, p, base);
-	if(mpz_probab_prime_p(temp, 25) > 0)
+	if(mpz_probab_prime_p(temp, 25) > 0 && temp > base)
 	{	//gmp_printf("%Zd is prime\n", temp);
 		mpz_clear(temp);
 		return 1;
@@ -472,15 +472,21 @@ int hasdivisor(family p)
 	emptyinstancestring(str, p);
 	mpz_set_str(empty, str, base);
 	mpz_set_str(gcd, str, base);
+        int allsingledigit = 1;
 	for(int i=0; i<p.len; i++)
 	{	for(int j=0; j<p.numrepeats[i]; j++)
 		{	instancestring(str, p, i, j);
 			mpz_set_str(temp, str, base);
 			mpz_gcd(gcd, gcd, temp);
+		        if(temp > base)
+				allsingledigit = 0;
 		}
 		if(p.numrepeats[i]>0)
 			numrepeats++;
 	}
+ 
+        if(allsingledigit)
+		return 1;
 
 	if(numrepeats==0)
 	{	
