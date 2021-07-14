c3038d273365052b27aad0372927902fb1a661dc
xayahrainie4793 <66528374+xayahrainie4793@users.noreply.github.com>
2021-07-14 19:31:21 +0800

    Update sieving.c

--- sieving.c
+++ sieving.c
@@ -25,15 +25,28 @@ int main(int argc, char** argv)
 		printf("merges the results with the LLR search data\n");
 		printf("\nNOTE: The program srsieve must be located in the directory srsieve\n");
 	}
+	else if(access("srsieve/srsieve", F_OK)==-1)
+		printf("The program srsieve must be located in the directory srsieve\n");
 	else if(argc>1)
 	{	signal(SIGINT, sig_handler);
+
+		char filename[25];
+		sprintf(filename, "srsieve/sieve.%s.txt", argv[1]);
+		if(access(filename, F_OK)==-1)
+		{	char filename2[25], copy[100];
+			sprintf(filename2, "data/sieve.%s.txt", argv[1]);
+			if(access(filename2, F_OK)==-1)
+			{	printf("No sieve file for base %s!\n", argv[1]);
+				return 0;
+			}
+			sprintf(copy, "cp %s %s", filename2, filename);
+			system(copy);
+		}
+
 		for(;;)
 		{	pid = fork();
 			if(pid==0)
-			{	char filename[25];
-				sprintf(filename, "srsieve/sieve.%s.txt", argv[1]);
 				execlp("timeout", "timeout", "120m", "srsieve/srsieve", filename, "-o", filename, NULL);
-			}
 			int status;
 			wait(&status);
 			system("./merge");
